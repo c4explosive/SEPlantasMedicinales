d6d28165c0dbcc5d4c0eeaf807dc8e1d3d1d6c0f
c4explosive <ao946391@gmail.com>
2017-07-02 20:07:58 -0500

    New Conclusions

--- NatMeK_ProySBC/NatMeK_ProySBC/p1_Inicio.Designer.vb
+++ NatMeK_ProySBC/NatMeK_ProySBC/p1_Inicio.Designer.vb
@@ -22,120 +22,120 @@ Partial Class p1_Inicio
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.pb_inicio = New System.Windows.Forms.PictureBox()
-        Me.lb_descripción = New System.Windows.Forms.Label()
-        Me.btn_consultar = New System.Windows.Forms.Button()
-        Me.btn_adm = New System.Windows.Forms.Button()
-        Me.lb_t2 = New System.Windows.Forms.Label()
-        Me.lb_t1 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
-        CType(Me.pb_inicio, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'pb_inicio
-        '
-        Me.pb_inicio.Image = Global.NatMeK_ProySBC.My.Resources.Resources.imageninicio
-        Me.pb_inicio.Location = New System.Drawing.Point(43, 152)
-        Me.pb_inicio.Name = "pb_inicio"
-        Me.pb_inicio.Size = New System.Drawing.Size(316, 250)
-        Me.pb_inicio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.pb_inicio.TabIndex = 2
-        Me.pb_inicio.TabStop = False
-        '
-        'lb_descripción
-        '
-        Me.lb_descripción.AutoSize = True
-        Me.lb_descripción.Font = New System.Drawing.Font("Lucida Sans", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lb_descripción.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.lb_descripción.Location = New System.Drawing.Point(387, 168)
-        Me.lb_descripción.Name = "lb_descripción"
-        Me.lb_descripción.Size = New System.Drawing.Size(0, 18)
-        Me.lb_descripción.TabIndex = 3
-        Me.lb_descripción.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'btn_consultar
-        '
-        Me.btn_consultar.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.btn_consultar.Font = New System.Drawing.Font("Lucida Sans", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btn_consultar.ForeColor = System.Drawing.Color.Black
-        Me.btn_consultar.Location = New System.Drawing.Point(390, 400)
-        Me.btn_consultar.Name = "btn_consultar"
-        Me.btn_consultar.Size = New System.Drawing.Size(127, 33)
-        Me.btn_consultar.TabIndex = 4
-        Me.btn_consultar.Text = "CONSULTAR"
-        Me.btn_consultar.UseVisualStyleBackColor = False
-        '
-        'btn_adm
-        '
-        Me.btn_adm.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.btn_adm.Font = New System.Drawing.Font("Lucida Sans", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btn_adm.ForeColor = System.Drawing.Color.Black
-        Me.btn_adm.Location = New System.Drawing.Point(530, 400)
-        Me.btn_adm.Name = "btn_adm"
-        Me.btn_adm.Size = New System.Drawing.Size(127, 33)
-        Me.btn_adm.TabIndex = 5
-        Me.btn_adm.Text = "ADMINISTRAR"
-        Me.btn_adm.UseVisualStyleBackColor = False
-        '
-        'lb_t2
-        '
-        Me.lb_t2.AutoSize = True
-        Me.lb_t2.Font = New System.Drawing.Font("Lucida Calligraphy", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lb_t2.ForeColor = System.Drawing.Color.Maroon
-        Me.lb_t2.Location = New System.Drawing.Point(420, 87)
-        Me.lb_t2.Name = "lb_t2"
-        Me.lb_t2.Size = New System.Drawing.Size(196, 45)
-        Me.lb_t2.TabIndex = 7
-        Me.lb_t2.Text = "NatMeK"
-        '
-        'lb_t1
-        '
-        Me.lb_t1.AutoSize = True
-        Me.lb_t1.Font = New System.Drawing.Font("Hobo Std", 28.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lb_t1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.lb_t1.Location = New System.Drawing.Point(102, 33)
-        Me.lb_t1.Name = "lb_t1"
-        Me.lb_t1.Size = New System.Drawing.Size(462, 54)
-        Me.lb_t1.TabIndex = 6
-        Me.lb_t1.Text = "NATURAL MEDICINE KIT"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(22, 441)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(437, 12)
-        Me.Label1.TabIndex = 8
-        Me.Label1.Text = "Elaborado por: Espinosa, Angel; González, Yorlenis; León, Valdir; Ojo, Karina"
-        '
-        'p1_Inicio
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(684, 461)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.lb_t2)
-        Me.Controls.Add(Me.lb_t1)
-        Me.Controls.Add(Me.btn_adm)
-        Me.Controls.Add(Me.btn_consultar)
-        Me.Controls.Add(Me.lb_descripción)
-        Me.Controls.Add(Me.pb_inicio)
-        Me.ForeColor = System.Drawing.Color.Green
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-        Me.MaximizeBox = False
-        Me.MinimizeBox = False
-        Me.Name = "p1_Inicio"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "BIENVENIDO"
-        CType(Me.pb_inicio, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.pb_inicio = New System.Windows.Forms.PictureBox()
+		Me.lb_descripción = New System.Windows.Forms.Label()
+		Me.btn_consultar = New System.Windows.Forms.Button()
+		Me.btn_adm = New System.Windows.Forms.Button()
+		Me.lb_t2 = New System.Windows.Forms.Label()
+		Me.lb_t1 = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		CType(Me.pb_inicio, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'pb_inicio
+		'
+		Me.pb_inicio.Image = Global.NatMeK_ProySBC.My.Resources.Resources.imageninicio
+		Me.pb_inicio.Location = New System.Drawing.Point(43, 152)
+		Me.pb_inicio.Name = "pb_inicio"
+		Me.pb_inicio.Size = New System.Drawing.Size(316, 250)
+		Me.pb_inicio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.pb_inicio.TabIndex = 2
+		Me.pb_inicio.TabStop = False
+		'
+		'lb_descripción
+		'
+		Me.lb_descripción.AutoSize = True
+		Me.lb_descripción.Font = New System.Drawing.Font("Lucida Sans", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lb_descripción.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.lb_descripción.Location = New System.Drawing.Point(387, 168)
+		Me.lb_descripción.Name = "lb_descripción"
+		Me.lb_descripción.Size = New System.Drawing.Size(0, 18)
+		Me.lb_descripción.TabIndex = 3
+		Me.lb_descripción.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'btn_consultar
+		'
+		Me.btn_consultar.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.btn_consultar.Font = New System.Drawing.Font("Lucida Sans", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btn_consultar.ForeColor = System.Drawing.Color.Black
+		Me.btn_consultar.Location = New System.Drawing.Point(390, 400)
+		Me.btn_consultar.Name = "btn_consultar"
+		Me.btn_consultar.Size = New System.Drawing.Size(127, 33)
+		Me.btn_consultar.TabIndex = 4
+		Me.btn_consultar.Text = "CONSULTAR"
+		Me.btn_consultar.UseVisualStyleBackColor = False
+		'
+		'btn_adm
+		'
+		Me.btn_adm.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.btn_adm.Font = New System.Drawing.Font("Lucida Sans", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btn_adm.ForeColor = System.Drawing.Color.Black
+		Me.btn_adm.Location = New System.Drawing.Point(530, 400)
+		Me.btn_adm.Name = "btn_adm"
+		Me.btn_adm.Size = New System.Drawing.Size(127, 33)
+		Me.btn_adm.TabIndex = 5
+		Me.btn_adm.Text = "ADMINISTRAR"
+		Me.btn_adm.UseVisualStyleBackColor = False
+		'
+		'lb_t2
+		'
+		Me.lb_t2.AutoSize = True
+		Me.lb_t2.Font = New System.Drawing.Font("Lucida Calligraphy", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lb_t2.ForeColor = System.Drawing.Color.Maroon
+		Me.lb_t2.Location = New System.Drawing.Point(420, 87)
+		Me.lb_t2.Name = "lb_t2"
+		Me.lb_t2.Size = New System.Drawing.Size(196, 45)
+		Me.lb_t2.TabIndex = 7
+		Me.lb_t2.Text = "NatMeK"
+		'
+		'lb_t1
+		'
+		Me.lb_t1.AutoSize = True
+		Me.lb_t1.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lb_t1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.lb_t1.Location = New System.Drawing.Point(102, 33)
+		Me.lb_t1.Name = "lb_t1"
+		Me.lb_t1.Size = New System.Drawing.Size(456, 44)
+		Me.lb_t1.TabIndex = 6
+		Me.lb_t1.Text = "NATURAL MEDICINE KIT"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Lucida Sans", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.Black
+		Me.Label1.Location = New System.Drawing.Point(22, 441)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(467, 12)
+		Me.Label1.TabIndex = 8
+		Me.Label1.Text = "Elaborado por: Espinosa, Angel; González, Yorlenis; León, Valdir; Ojo, Karina"
+		'
+		'p1_Inicio
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+		Me.ClientSize = New System.Drawing.Size(684, 461)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.lb_t2)
+		Me.Controls.Add(Me.lb_t1)
+		Me.Controls.Add(Me.btn_adm)
+		Me.Controls.Add(Me.btn_consultar)
+		Me.Controls.Add(Me.lb_descripción)
+		Me.Controls.Add(Me.pb_inicio)
+		Me.ForeColor = System.Drawing.Color.Green
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "p1_Inicio"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "BIENVENIDO"
+		CType(Me.pb_inicio, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
-    Friend WithEvents pb_inicio As PictureBox
+	End Sub
+	Friend WithEvents pb_inicio As PictureBox
     Friend WithEvents lb_descripción As Label
     Friend WithEvents btn_consultar As Button
     Friend WithEvents btn_adm As Button
--- NatMeK_ProySBC/NatMeK_ProySBC/p1_Inicio.vb
+++ NatMeK_ProySBC/NatMeK_ProySBC/p1_Inicio.vb
@@ -1,21 +1,46 @@
-﻿Public Class p1_Inicio
-    Private Sub p1_Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        lb_descripción.Text = " " & vbCrLf &
-                              "NatMek es un sistema que orienta" & vbCrLf &
-                              "en  la  utilización  de  plantas" & vbCrLf &
-                              "medicinales en el tratamiento de" & vbCrLf &
-                              "padecimientos comunes, es decir," & vbCrLf &
-                              "que no son de carácter crónico." & vbCrLf &
-                              " " & vbCrLf &
-                              " " & vbCrLf &
-                              " " & vbCrLf &
-                              "Para empezar, elija una de las" & vbCrLf &
-                              "siguientes opciones:"
-    End Sub
+﻿Imports SbsSW.SwiPlCs
 
-    Private Sub btn_consultar_Click(sender As Object, e As EventArgs) Handles btn_consultar.Click
-        Me.Hide()
-        p2_área.Show()
+Public Class p1_Inicio
+	Private Sub p1_Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		lb_descripción.Text = " " & vbCrLf &
+							  "NatMek es un sistema que orienta" & vbCrLf &
+							  "en  la  utilización  de  plantas" & vbCrLf &
+							  "medicinales en el tratamiento de" & vbCrLf &
+							  "padecimientos comunes, es decir," & vbCrLf &
+							  "que no son de carácter crónico." & vbCrLf &
+							  " " & vbCrLf &
+							  " " & vbCrLf &
+							  " " & vbCrLf &
+							  "Para empezar, elija una de las" & vbCrLf &
+							  "siguientes opciones:"
+	End Sub
 
-    End Sub
+	Private Sub btn_consultar_Click(sender As Object, e As EventArgs) Handles btn_consultar.Click
+		'Me.Hide()
+		Dim rad_array As RadioButton()
+		ReDim rad_array(990)
+		Dim i As Integer
+		i = 0
+		Dim y As Integer = 10
+		connect_to_base()
+		Dim loadr As PlQuery = New PlQuery("cargar('avescorredoras.bd')")
+		loadr.NextSolution()
+		Dim avescorr As PlQuery = New PlQuery("son_aves_corredoras(X)")
+		For Each z As PlQueryVariables In avescorr.SolutionVariables
+			rad_array(i) = New RadioButton
+			'lstResponse.Items.Add(z("X").ToString)
+			rad_array(i).Text = z("X").ToString
+			rad_array(i).Location = New Point(0, y)
+			Me.Controls.Add(rad_array(i))
+			i += 1
+			y += 100
+			Debug.Print(z("X").ToString)
+		Next
+		'p2_área.Show()
+
+	End Sub
+
+	Private Sub pb_inicio_Click(sender As Object, e As EventArgs) Handles pb_inicio.Click
+
+	End Sub
 End Class
